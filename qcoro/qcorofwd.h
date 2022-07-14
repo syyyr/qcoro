c612f7f052427ef033e9d236c76ee4aee98ba31b
Daniel Vrátil <dvratil@kde.org>
2022-07-14 12:51:40 +0200

    Introduce AbortOnException option to QCoro::Task
    Adds an additional optinal template argument to QCoro::Task,
    which may specify an AbortOnException option. When the option
    is set on a coroutine and the coroutine throws an exception,
    QCoro will abort the program, UNLESS the coroutine is being
    co_awaited, which is the only way to propagate the exception
    from a coroutine.

--- qcoro/qcorofwd.h
+++ qcoro/qcorofwd.h
@@ -4,9 +4,27 @@
 
 #pragma once
 
+#include <tuple>
+
 namespace QCoro {
 
-template<typename T> class Task;
+namespace Options {
+    struct AbortOnException;
+} // namespace Options
+
+template<typename T>
+concept TaskOption =
+    std::is_same_v<T, Options::AbortOnException>;
+
+template<TaskOption ...Opts>
+struct TaskOptions;
+
+struct TaskOptionsStorage;
+
+template<TaskOption ... Opts>
+struct TaskOptions;
+
+template<typename T = void, typename Opts = TaskOptions<>> class Task;
 template<typename T> class Generator;
 template<typename T> class GeneratorIterator;
 template<typename T> class AsyncGenerator;
